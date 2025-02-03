596f5a009c1288ca101858eefacb80981d527944
ratmi <ratmi@>
2025-02-03 11:33:10 +0100

    Add project files.

---
+++ Test try out/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_try_out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //begin codeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
